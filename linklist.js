02f1ab5be80b0d06b8ea19a8bab8de36b27a4fc6
Chandan Janardan <chandanjanardan@gmail.com>
2023-05-23 08:21:12 +0530

    adding double link list

--- linklist.js
+++ linklist.js
@@ -32,7 +32,7 @@ function insert(newElement,item){
 function display(){
     var currNode=this.head
     while(!(currNode.next==null)){
-        console.log(currNode.next.element);
+        
         currNode=currNode.next
     }
 }
@@ -49,6 +49,7 @@ function display(){
 function findPrevious(item){
     var currNode=this.head;
     while((currNode.next!==null &&(currNode.next.element !=item))){
+        
         currNode=currNode.next
     }
     return currNode
