1716df3dd93e98da694b98abbf68be80ce82107d
Chandan Janardan <chandanjanardan@gmail.com>
2023-05-24 23:41:48 +0530

    adding codes

---
+++ GPT/prac.js
@@ -0,0 +1,44 @@
+// node creation 
+function Node(value){
+    this.value=value
+    this.next=null
+}
+// Linklist makeing 
+
+function Linklist(){
+    this.head=null
+}
+
+// append
+Linklist.prototype.prepend=function(value){
+    var newNode= new Node(value)
+    newNode.next=this.head
+    this.head=newNode
+}
+Linklist.prototype.append=function(value){
+    var newNode=new Node(value)
+    if(!this.head){
+        this.head=newNode
+    }else{
+    var currNode=this.head
+        while((currNode.next)){
+            currNode=currNode.next
+        }
+        currNode.next=newNode
+    }
+}
+
+Linklist.prototype.display=function(){
+    var currNode=this.head
+    var list=[]
+    while(currNode){
+        list.push(currNode.value)
+        currNode=currNode.next
+    }
+    console.log(list.join("->"))
+}
+
+const num=new Linklist()
+num.append(10)
+num.append(20)
+num.display()
