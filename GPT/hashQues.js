6f147e085ad64c017a4296149df36473de0e4986
Chandan Janardan <chandanjanardan@gmail.com>
2023-06-27 23:22:01 +0530

    adding hash question

--- GPT/hashQues.js
+++ GPT/hashQues.js
@@ -23,4 +23,32 @@ function findFirstNonRepeatedChar(str) {
   const input = "abacabad";
   const result = findFirstNonRepeatedChar(input);
   console.log(result); // Output: "c"
+
+
+
+
+
+
+
+  function twoSum(nums, target) {
+  const numMap = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    
+    if (numMap.hasOwnProperty(complement)) {
+      return [numMap[complement], i];
+    }
+    
+    numMap[nums[i]] = i;
+  }
+  
+  return null; // No two numbers add up to the target sum
+}
+
+const input = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(input, target);
+console.log(result); // Output: [0, 1]
+
   
