26ac48a0b949b8d867d3dbe41acd1952e893cb90
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-31 21:49:32 +0530

    adding fetch

--- GPT/async.js
+++ GPT/async.js
@@ -12,8 +12,19 @@ function doSomething(){
 // },(err)=>{
 //     console.log(err)
 // })
-doSomething().then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-console.log(err)
+// doSomething().then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+// console.log(err)
+// })
+
+
+
+// fetch api-------------------------------*_________________________________
+
+
+fetch("https://fakestoreapi.com/products").then((response)=>{})
+        console.log(response)
+.catch((err)=>{
+    console.log(err,"some error in data")
 })
