0936ae658b7cd9333b54d192f20727a336554672
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-31 21:42:23 +0530

    adding .then and .catch

--- GPT/async.js
+++ GPT/async.js
@@ -1,14 +1,19 @@
 // promise 
 
 
-const doSomething=()=>{
+function doSomething(){
     return new Promise((resolve,reject)=>{
         // resolve("promise is resolved")
         reject('promsie is reject')
     })
 }
+// doSomething().then((data)=>{
+//     console.log(data)
+// },(err)=>{
+//     console.log(err)
+// })
 doSomething().then((data)=>{
     console.log(data)
-},(err)=>{
-    console.log(err)
+}).catch((err)=>{
+console.log(err)
 })
