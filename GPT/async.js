b5f966420dc8a99859230ea73405008a60347db5
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-31 21:38:46 +0530

    adding catch

--- GPT/async.js
+++ GPT/async.js
@@ -3,9 +3,12 @@
 
 const doSomething=()=>{
     return new Promise((resolve,reject)=>{
-        resolve("promise is resolved")
+        // resolve("promise is resolved")
+        reject('promsie is reject')
     })
 }
 doSomething().then((data)=>{
     console.log(data)
+},(err)=>{
+    console.log(err)
 })
