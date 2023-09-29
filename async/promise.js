ea2a920a3594fb87f64a54138837f3704b289540
Chandan Janardan <chandanjanardan@gmail.com>
2023-09-29 13:17:13 +0530

    chaninng .then

--- async/promise.js
+++ async/promise.js
@@ -1,11 +1,23 @@
 
+// const promise=new Promise((resolve,reject)=>{
+//     // resolve("Yehhh boy")
+//     reject("shit boy")
+// })
+// promise.then(data=>(
+//     console.log(data)
+// ))
+// .catch(data=>(
+//     console.log(data)
+// ))
+
+
+// -------------multiple .then
+
 const promise=new Promise((resolve,reject)=>{
-    // resolve("Yehhh boy")
-    reject("shit boy")
+    resolve({user:"Chandy"})
 })
-promise.then(data=>(
-    console.log(data)
-))
-.catch(data=>(
-    console.log(data)
-))
+promise.then((data)=>{
+    return {...data,age:21}
+}).then((user)=>{
+    console.log(user)
+})
