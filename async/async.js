48236e1dc0d1202718fdf0c969c53e507d77429d
Chandan Janardan <chandanjanardan@gmail.com>
2023-10-01 18:37:55 +0530

    adding bubblesort practice

--- async/async.js
+++ async/async.js
@@ -1,6 +1,20 @@
 // async function ke andar jo bhai hai wo promise return karta hai
+// async function getData(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("Hello")
+//         },2000)
+//     })
+// }
+// const data= getData()
+// // console.log(data)
+// data.then((res)=>console.log(res))
+const promise=new Promise((resolve,reject)=>{
+           setTimeout(()=>{
+               resolve("Hello is resolved")
+           },2000)})
 async function getData(){
-    
+    const data=await promise
+    console.log(data)
 }
-const data= getData()
-console.log(data)
+getData()
