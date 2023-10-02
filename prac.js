661e5458de131f0a09718f9ef4a0022cc82417ae
Chandan Janardan <chandanjanardan@gmail.com>
2023-10-02 13:40:06 +0530

    adding prac quicksort

--- prac.js
+++ prac.js
@@ -1,18 +1,21 @@
-function bubbleSort(arr){
-    let swapped
-    do{
-        swapped=false
-        for (let i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
-                let temp=arr[i]
-                arr[i]=arr[i+1]
-                arr[i+1]=temp
-                swapped=true
-            }
+function quickSort(arr){
+    if(arr.length<2){
+        return arr
+    }
+    let last=arr.length-1
+    let pivot=arr[last]
+    let left=[]
+    let right =[]
+    for (let i=0;i<last;i++){
+        if(arr[i]<pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
         }
-    }while(swapped)
-    return arr
+    }
+    left=quickSort(left)
+    right=quickSort(right)
+    return [...left,pivot,...right]
 }
-let arr=[5,3,2,6,1]
-const sorted=bubbleSort(arr)
-console.log(sorted)
+let arr=[-6,20,8-2,4]
+console.log(quickSort(arr))
