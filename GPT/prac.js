417755ce23ed2ea1aac5db7b118a87d781544df5
Chandan Janardan <chandanjanardan@gmail.com>
2023-10-05 19:17:06 +0530

    ading tree prac

--- GPT/prac.js
+++ GPT/prac.js
@@ -1,15 +1,1 @@
-// Binary search 
-let arr=[3,3,4,5,6,7]
-function search(arr,target){
-    
-    let i=0
-    while(arr[i]<arr.length-1){
-        if(target==arr[i]){
-            console.log(i)
-            i++
-            
-        }
-    }
-    return -1
-}
-search(arr,4)
+function 
