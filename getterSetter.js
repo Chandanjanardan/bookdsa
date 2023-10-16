e10e132b33cb198c53fe10ce78c3fd44d8321484
Chandan Janardan <chandanjanardan@gmail.com>
2023-10-16 17:08:54 +0530

    adding getter and setter

---
+++ getterSetter.js
@@ -0,0 +1,38 @@
+// let obj={
+//     name:"Chandan",
+//    get greet(){
+//         console.log(`Hello ${this.name}`)
+//     }
+// }
+// so initially when we want to acces the greet method we have to use ()
+// but by seting it to get we can use it as property
+// console.log(obj.greet)
+
+// let obj={
+//     name:"CHandan"
+
+// }
+// function greet(){
+//     console.log(`${this.name} hello`)
+// }
+// // greet.call(obj)
+// let greeting=greet.bind(obj)
+// greeting()
+// this is call and bind
+
+
+//now get back to setter
+let obj={
+    name:"Chandan",
+   get greet(){
+        console.log(`Hello ${this.name}`)
+    },
+    set greet(value){
+        this.name=value
+        console.log(`hello ${this.name}`)
+    }
+}
+// I want to chagange greet method
+// If i have to change the value from out side we can pass it through setter
+obj.greet
+
--- prac.js
+++ prac.js
@@ -1,13 +1,1 @@
-let people={
-    name:"Chandan",
-    id:2
-}
-// const obj=Object.entries(people)
-
-// console.log(obj[0][1])
-const arrayOfObj=Object.entries(people)
-console.log(arrayOfObj)
-arrayOfObj.map(([key,value])=>({
-    key:key,
-    value:value
-}))
+// getter and setter 
