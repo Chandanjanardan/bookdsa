2eb47e0fa224b3e9d66f6aed7663e4725686a978
Chandan Janardan <chandanjanardan@gmail.com>
2023-11-10 19:01:59 +0530

    adding

--- prac.js
+++ prac.js
@@ -2,3 +2,6 @@
 let arr=[1,1,2,3,4,4,4,5,5,6,6]
 let unique = arr.filter((el,idx)=>arr.indexOf(el)==idx)
 console.log(unique)
+let arr=[1,1,2,3,4,4,4,5,5,6,6]
+let unique = arr.filter((el,idx)=>arr.indexOf(el)==idx)
+console.log(unique)
